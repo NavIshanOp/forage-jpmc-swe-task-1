4d69eddd630e1d41f6dd09c65aab6a64cf2b18f8
NavIshanOp <ishan.jaiswal@hotmail.com>
2024-08-05 12:16:17 +0530

    Fix price calculation and ratio calculation in client3.py and add unit tests

--- client3.py
+++ client3.py
@@ -56,3 +56,4 @@ if __name__ == "__main__":
 
         if 'ABC' in prices and 'DEF' in prices:
             print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
+            
--- client_test.py
+++ client_test.py
@@ -42,4 +42,5 @@ class Client3Test(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
     
